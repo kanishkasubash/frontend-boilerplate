893cdcebfb030e74e90573fc42e277b9be991599
kanishkasubash <kanishkasubash@gmail.com>
2023-07-30 03:55:54 +0530

    modified:   dist/css/style.min.css 	modified:   dist/css/style.min.css.map 	new file:   dist/index.html 	modified:   gulpfile.js 	modified:   package-lock.json 	modified:   package.json 	new file:   src/images/.keep 	modified:   src/index.html 	modified:   src/scss/globles/colors.scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,123 @@
 // Importing specific gulp API functions
-const { src, dest, watch, series, parallel } = require('gulp');
+const { src, dest, watch, series, parallel } = require("gulp");
 
 // Import all other Gulp-related packages
-const rename = require('gulp-rename');
-const sass = require('gulp-sass')(require('sass'));
-const uglify = require('gulp-uglify');
-const autoPrefixer = require('gulp-autoprefixer');
-const sourcemaps = require('gulp-sourcemaps');
-const browserify = require('browserify');
-const babelify = require('babelify');
-const source = require('vinyl-source-stream');
-const buffer = require('vinyl-buffer');
+const rename = require("gulp-rename");
+const sass = require("gulp-sass")(require("sass"));
+const uglify = require("gulp-uglify");
+const autoPrefixer = require("gulp-autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const plumber = require('gulp-plumber');
+const browserify = require("browserify");
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
 const browsersync = require("browser-sync").create();
 
-// File paths
-const styleSrc = 'src/scss/style.scss';
-const styleDist = './dist/css/';
+// src file paths
+const styleSrc = "./src/scss/style.scss";
+const scriptSrc = "script.js";
+const htmlSrc = "./src/**/*.html";
+const imgSrc = "./src/images/**/*.*"
 
-const scriptSrc = 'script.js';
-const scriptFolder = 'src/js/';
-const scriptDist = './dist/js/';
+// dist file paths
+const styleDist = "./dist/css/";
+const scriptDist = "./dist/js/";
+const htmlDist = "./dist/";
+const imgDist = "./dist/images/";
 
-const styleWatch = 'src/scss/**/*.scss';
-const scriptWatch = 'src/js/**/*.js';
-const htmlWatch = 'src**/*.html';
+// watch file paths
+const styleWatch = "./src/scss/**/*.scss";
+const scriptWatch = "./src/js/**/*.js";
+const htmlWatch = "./src/**/*.html";
+const imgWatch = "./src/images/**/*.*";
 
-// JS Files as an array
+// JS Files & Folders
 const scriptFiles = [scriptSrc];
+const scriptFolder = "./src/js/";
 
 // browserSync to run a local server & auto reload HTTPS
 function browserSync() {
     browsersync.init({
         server: {
-            baseDir: './src'
-        }
+            baseDir: "./dist/",
+        },
     });
 }
 
 // browserSyncReload
-function browserSyncReload(done) {
+function browserSyncReload(callBack) {
     browsersync.reload();
-    done();
+    callBack();
 }
 
 // style task: compiles SCSS to CSS and put final style.min.css file into dist/css folder
-function style() {
+function style(callBack) {
     return src(styleSrc)
         .pipe(sourcemaps.init())
-        .pipe(sass({
-            errorLogToConsole: true,
-            outputStyle: 'compressed'
-        }))
-        .on('error', console.error.bind(console))
-        .pipe(autoPrefixer({
-            overrideBrowserslist: ['last 2 versions', '> 5%', 'Firefox ESR'],
-            cascade: false
-        }))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(sourcemaps.write('./'))
+        .pipe(
+            sass({
+                errorLogToConsole: true,
+                outputStyle: "compressed",
+            })
+        )
+        .on("error", console.error.bind(console))
+        .pipe(
+            autoPrefixer({
+                overrideBrowserslist: ['last 2 versions', '> 5%', 'Firefox ESR'],
+                cascade: false,
+            })
+        )
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(sourcemaps.write("./"))
         .pipe(dest(styleDist))
         .pipe(browsersync.stream());
+    callBack();
 }
 
 // script task: compiles and bundle JS files and put final script.min.js file into dist/js folder
-async function script() {
+async function script(callBack) {
     scriptFiles.map(function (entry) {
         return browserify({
-            entries: [scriptFolder + entry]
+            entries: [scriptFolder + entry],
         })
-            .transform(babelify, { presets: ['@babel/preset-env'] })
+            .transform(babelify, { presets: ["@babel/preset-env"] })
             .bundle()
             .pipe(source(entry))
-            .pipe(rename({ extname: '.min.js' }))
+            .pipe(rename({ extname: ".min.js" }))
             .pipe(buffer())
             .pipe(sourcemaps.init({ loadmaps: true }))
             .pipe(uglify())
-            .pipe(sourcemaps.write('./'))
+            .pipe(sourcemaps.write("./"))
             .pipe(dest(scriptDist))
             .pipe(browsersync.stream());
     });
+    callBack();
+}
+
+// runPlumber
+function runPlumber(srcFile, destFile) {
+    return src(srcFile)
+        .pipe(plumber())
+        .pipe(dest(destFile));
+}
+
+function html() {
+    return runPlumber(htmlSrc, htmlDist);
+}
+
+function images() {
+    return runPlumber(imgSrc, imgDist);
 }
 
 // watchFiles task: watch change(s) of HTML, SCSS and JS files.
 //If any change(s), run style and script tasks simultaneously
 function watchFiles() {
-    watch(htmlWatch, browserSyncReload);
-    watch([styleWatch, scriptWatch], series(style, script));
+    watch([styleWatch, scriptWatch, htmlWatch, imgWatch], series(style, script, html, images, browserSyncReload));
 }
 
-// Export the default Gulp task to run
-exports.default = series(parallel(style, script), parallel(browserSync, watchFiles));
+//Export the default Gulp task to run
+exports.default = series(
+    parallel(style, script, html, images),
+    parallel(browserSync, watchFiles)
+);
